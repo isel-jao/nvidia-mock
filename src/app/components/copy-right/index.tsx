c4ce271788ae8c44a00fbafce908219f55edce14
oussamajamil <oussamajamil01@gmail.com>
2024-11-20 12:16:30 +0100

    footor nd copy right

---
+++ src/app/components/copy-right/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Logo from "../logo";
+
+function CopyRightComponent() {
+  // Privacy Policy Manage My Privacy Do Not Sell or Share My Data Terms of Service Accessibility Corporate Policies Product Security Contact
+  const urls = [
+    { name: "Privacy Policy", url: "#" },
+    { name: "Manage My Privacy", url: "#" },
+    { name: "Do Not Sell or Share My Data", url: "#" },
+    { name: "Terms of Service", url: "#" },
+    { name: "Accessibility", url: "#" },
+    { name: "Corporate Policies", url: "#" },
+    { name: "Product Security", url: "#" },
+    { name: "Contact", url: "#" },
+  ];
+  return (
+    <div className="container flex flex-col gap-4 bg-background">
+      <div className="flex justify-between">
+        <Logo fill="fill-black/70" />
+        <span className="text-xl">Morocco</span>
+      </div>
+      <div className="flex flex-wrap justify-between gap-4 text-foreground/70">
+        {urls.map((url, index) => (
+          <a key={index} href={url.url} className={`hover:text-foreground/100`}>
+            {url.name}
+            {index !== urls.length - 1 && (
+              <span className="px-2 text-foreground/70">|</span>
+            )}
+          </a>
+        ))}
+      </div>
+      <div className="flex justify-between text-foreground/70">
+        <span>
+          ©{new Date().getFullYear()} NVIDIA Corporation. All Rights Reserved.
+        </span>
+        <span>Legal Information</span>
+      </div>
+    </div>
+  );
+}
+export default CopyRightComponent;
---
+++ src/app/components/footer/index.tsx
@@ -0,0 +1,83 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function Footer({
+  data,
+  children,
+  title,
+  links,
+}: {
+  data: {
+    title: string;
+    links: {
+      name: string;
+      redirect: string;
+    }[];
+  }[];
+  children: React.ReactNode;
+  title: string;
+  links: any[];
+}) {
+  return (
+    <div className="dark flex flex-col gap-4 bg-background text-foreground">
+      <div className="container flex flex-col gap-[2rem]">
+        <div className="grid gap-8 md:grid-cols-3">
+          {data.map((item, index) => (
+            <div key={index}>
+              <div className="mb-[2rem] border-b-2 border-foreground/40 pb-3 text-xl font-bold">
+                <h1 className="">{item.title}</h1>
+              </div>
+              <ul className="space-y-3">
+                {item.links.map(
+                  (
+                    link: {
+                      name: string;
+                      redirect: string;
+                    },
+                    index: number,
+                  ) => (
+                    <li
+                      key={index}
+                      className="text-[#76b900] hover:text-[#8cc722]"
+                    >
+                      <a href={link.redirect}>{link.name}</a>
+                    </li>
+                  ),
+                )}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div className="flex min-h-[4rem] flex-wrap items-center justify-between">
+          {children}
+          <div className="flex gap-2">
+            <span className="text-xl font-light">{title}</span>
+            {
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              links.map(
+                (
+                  link: {
+                    icon: React.ReactNode;
+                    redirect: string;
+                  },
+                  index: number,
+                ) => (
+                  <a
+                    key={index}
+                    href={link.redirect}
+                    className="size-[1rem] opacity-50 transition-opacity hover:opacity-100 md:size-[2rem]"
+                  >
+                    {link.icon}
+                  </a>
+                ),
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
