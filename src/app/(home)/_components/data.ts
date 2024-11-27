5680afa3d8329955d6032439c285707ba173fa9d
oussamajamil <oussamajamil01@gmail.com>
2024-11-27 11:20:39 +0100

    fixed

--- src/app/(home)/_components/data.ts
+++ src/app/(home)/_components/data.ts
@@ -1,4 +1,237 @@
+
 export const slide1Data = {
+  title: "Artificial Intelligence",
+  subtitle:
+    "AI is powering change in every industry. From generative AI and speech recognition to medical imaging and improved supply chain management, AI is providing enterprises the compute power, tools, and algorithms their teams need to do their life's work.",
+    slides: [
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374/item_1721986382382.coreimg.svg/1732299019452/nvidia-and-aws-partnership-v.svg",
+        href: "/blogs/blog-1",
+        tag: "Generative AI",
+        type:"Blog",
+        title: "NVIDIA at Aws re:Invent 2024",
+        subtitle:
+          "Join NVIDIA at AWS at re:Invent to learn about the latest breakthroughs in generative AI.",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374/item_1722403062839.coreimg.100.850.jpeg/1732299019469/ai-agent-customer-service-rep.jpeg",
+        tag: "Generative AI",
+        type:"Blog",
+        href: "/blogs/blog-2",
+        title: "Efficiency Meets Personalization: AI Agents Boost Customer Service",
+        subtitle:
+          "Efficiency Meets Personalization: AI Agents Boost Customer Service",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374/item_1722277565106.coreimg.100.850.jpeg/1732299019486/slackbot-llm-sfg.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        href: "/blogs/blog-3",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374/nv_teaser_copy_2006776514.coreimg.100.850.jpeg/1732299019502/sfg-softbank-ai-jp.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        href: "/blogs/blog-4",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },{
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374/item_1691512647007.coreimg.100.850.jpeg/1732299019566/japan-ai-summit-healthcare-and-robotics-multi-image-ari.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        href: "/blogs/blog-5",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },
+      
+    ],
+    quickLinks: [
+      {
+        name: "NVIDIA Omniverse",
+        href: "/nvidia-omniverse",
+      },
+      {
+        name: "NVIDIA NIM",
+        href: "/nvidia-nim",
+      },
+      {
+        name: "NVIDIA Isaac",
+        href: "/nvidia-isaac",
+      },
+      {
+        name: "NVIDIA Jetson",
+        href: "/nvidia-jetson",
+      },
+    ],
+};
+
+export const slide2Data = {
+  title: "Design and Simulation",
+  subtitle:
+    "NVIDIA RTX™ and NVIDIA Omniverse™ deliver the performance to help professionals, creators, developers, and students worldwide enhance creative workflows and build, operate, and connect metaverse applications.",
+    slides: [
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/Copy%20of%20nv_carousel_10568374/item_1669209935575.coreimg.100.850.jpeg/1732217612170/nov-ito-cava-ari.jpeg",
+        tag: "Simulation",
+        type:"Blog",
+        href: "/blogs/blog-1",
+        title: " Generative AI-Enabled Content Creation With OpenUSD",
+        subtitle:
+          "NVIDIA Omniverse™ devs are enabling brands to deliver personalized, high-quality visuals at scale.",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/Copy%20of%20nv_carousel_10568374/item_1676348880688.coreimg.svg/1732217612421/nvidia-and-microsoft-partnership-h-on-dark.svg",
+        tag: "Generative AI",
+        type:"Blog",
+        href: "/blogs/blog-2",
+        title: "Efficiency Meets Personalization: AI Agents Boost Customer Service",
+        subtitle:
+          "Efficiency Meets Personalization: AI Agents Boost Customer Service",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/Copy%20of%20nv_carousel_10568374/item_1669209933306.coreimg.100.850.jpeg/1732217612556/foxconn-sfg.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        href: "/blogs/blog-3",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/Copy%20of%20nv_carousel_10568374/item_1710799952318.coreimg.100.850.jpeg/1732217612788/openusd-1920x1080.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        href: "/blogs/blog-4",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },{
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/Copy%20of%20nv_carousel_10568374/nv_teaser_copy.coreimg.100.850.jpeg/1732217612805/wpp-coca-cola-ari.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        href: "/blogs/blog-5",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },
+      
+    ], 
+    quickLinks: [
+      {
+        name: "NVIDIA Omniverse",
+        href: "/nvidia-omniverse",
+      },
+      {
+        name: "NVIDIA NIM",
+        href: "/nvidia-nim",
+      },
+      {
+        name: "NVIDIA Isaac",
+        href: "/nvidia-isaac",
+      },
+      {
+        name: "NVIDIA Jetson",
+        href: "/nvidia-jetson",
+      },
+    ],
+};
+
+export const slide3Data = {
+  title: "High-Performance Computing",
+  subtitle:
+    "High-performance computing (HPC) is the ability to process data and perform complex calculations at high speeds. HPC is one of the most essential tools fueling the advancement of computational science.",
+    slides: [
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374_662431768/item_1731658011588.coreimg.100.850.jpeg/1732217615149/sc24-hpc-earth-2-sfg.jpeg",
+        tag: "HPC",
+        type:"Announcement",
+        href: "/blogs/blog-1",
+        title: "Earth-2 Announces New ",
+        subtitle:
+          "NVIDIA Omniverse™ devs are enabling brands to deliver personalized, high-quality visuals at scale.",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374_662431768/nv_teaser_copy_20067.coreimg.100.850.jpeg/1732217615166/hpc-visual-alchemi-bgr-nim-sc24-ari.jpeg",
+        tag: "Generative AI",
+        type:"Blog",
+        href: "/blogs/blog-2",
+        title: "Efficiency Meets Personalization: AI Agents Boost Customer Service",
+        subtitle:
+          "Efficiency Meets Personalization: AI Agents Boost Customer Service",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374_662431768/item_1730558280916.coreimg.100.850.jpeg/1732217615435/supercomputing-24-special-address-sfg-new.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        href: "/blogs/blog-3",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },
+      {
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374_662431768/item_1715357004595.coreimg.100.850.jpeg/1732217615451/quantum-computing-isc-sfg-1920x1080.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        href: "/blogs/blog-4",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },{
+        image: "https://www.nvidia.com/en-us/home/_jcr_content/root/responsivegrid/nv_carousel_10568374_662431768/item_1706106769086.coreimg.100.850.jpeg/1732217615467/nvidia-nsf-nair-sfg.jpeg",
+        tag: "Artificial Intelligence",
+        type:"Blog",
+        href: "/blogs/blog-5",
+        title: "Enhance Workflows With a Custom Slackbot LLM Agent",
+        subtitle:
+          "Optimize productivity and communication with a custom Slackbot built on NVIDIA NIM and LangChain.",
+      },
+      
+    ], 
+    quickLinks: [
+      {
+        name: "NVIDIA Omniverse",
+        href: "/nvidia-omniverse",
+      },
+      {
+        name: "NVIDIA NIM",
+        href: "/nvidia-nim",
+      },
+      {
+        name: "NVIDIA Isaac",
+        href: "/nvidia-isaac",
+      },
+      {
+        name: "NVIDIA Jetson",
+        href: "/nvidia-jetson",
+      },
+    ],
+};
+
+export const slide4Data = {
+  title: "Robotics and Edge AI",
+  subtitle:
+    "The NVIDIA Jetson™ and Isaac™ platforms provide end-to-end solutions to develop and deploy AI-powered autonomous machines and edge computing applications across manufacturing, logistics, healthcare, smart cities, and retail.",
+  slides: Array.from({ length: 10 }).map((_, i) => ({
+    image: `/images/placeholder.svg`,
+    tag: "tag",
+    type: "type",
+    title: "NVIDIA Advances Robot Learning With AI and Simulation Tools",
+    subtitle:
+      "New robot learning workflows and AI world model development tools can help robotics developers accelerate their AI-enabled robot and humanoid projects.",
+    href: `/blogs/blog-${i}`,
+  })),
+  quickLinks: Array.from({ length: 10 }).map((_, i) => ({
+    name: `quick link ${i}`,
+    href: `/quick-link-${i}`,
+  })),
+};
+
+export const slide5Data = {
   title: "Robotics and Edge AI",
   subtitle:
     "The NVIDIA Jetson™ and Isaac™ platforms provide end-to-end solutions to develop and deploy AI-powered autonomous machines and edge computing applications across manufacturing, logistics, healthcare, smart cities, and retail.",
