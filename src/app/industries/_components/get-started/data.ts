a461cbd6cb729ff64cf035fc1bee170304757ad9
Issam El jaouhary <iseljao89@gmail.com>
2024-11-27 14:49:51 +0100

    industries (get started section)

---
+++ src/app/industries/_components/get-started/data.ts
@@ -0,0 +1,36 @@
+export const data = {
+  title: "Get Started",
+  description: "Take the Next Steps",
+  items: [
+    {
+      title: "Develop Solutions to the Most Complex Challenges",
+      description:
+        "Access hundreds of GPU-accelerated containers, models, and SDKs—all the tools necessary to successfully build apps with NVIDIA technology.",
+      link: {
+        type: "default",
+        url: "#",
+        text: "Start Building",
+      },
+    },
+    {
+      title: "Discover Training to Meet Your Organization's Goals",
+      description:
+        "Elevate your team's skills in AI, graphics and simulation, accelerated computing, and more with custom training plans, team pricing, and flexible delivery options.",
+      link: {
+        type: "outline",
+        url: "#",
+        text: "Start Learning",
+      },
+    },
+    {
+      title: "Re-Envision the Future of Your Business",
+      description:
+        "Explore resources for modern business leaders, including the guidance they need to innovate, mitigate risk, and capitalize on AI and accelerated computing opportunities.",
+      link: {
+        type: "outline",
+        url: "#",
+        text: "Learn More",
+      },
+    },
+  ],
+};
--- src/app/industries/_components/get-started/index.tsx
+++ src/app/industries/_components/get-started/index.tsx
@@ -1,10 +1,40 @@
 import React from "react";
 import { links } from "../navbar/data";
+import { data } from "./data";
+import { CustomLink } from "@/app/components/custom-link";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function GetStartedSection() {
   return (
     <section id={links[3].href.slice(1)}>
-      <main className="mx-auto w-full max-w-screen-2xl p-container"></main>
+      <main className="mx-auto w-full max-w-screen-2xl p-container">
+        <h2 className="text-center text-xl font-bold">{data.title} </h2>
+        <h2 className="mt-8 text-center text-4xl font-bold">
+          {data.description}{" "}
+        </h2>
+        <ul className="mt-8 grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] gap-4">
+          {data.items.map((item, index) => (
+            <li key={index} className="space-y-4">
+              <h3 className="text-2xl font-bold">{item.title}</h3>
+              <p>{item.description}</p>
+              <div>
+                {item.link.type === "outline" ? (
+                  <CustomLink
+                    className="font-bold"
+                    href={item.link.url}
+                    name={item.link.text}
+                  />
+                ) : (
+                  <Link href={item.link.url}>
+                    <Button size="lg">{item.link.text}</Button>
+                  </Link>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
     </section>
   );
 }
