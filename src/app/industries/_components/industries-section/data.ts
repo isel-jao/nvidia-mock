d4e0942d8e1efcce364dd2340cc8362f08c67ca3
Issam El jaouhary <iseljao89@gmail.com>
2024-11-27 12:29:27 +0100

    industris

---
+++ src/app/industries/_components/industries-section/data.ts
@@ -0,0 +1,122 @@
+export const data: {
+  description: string;
+  industries: {
+    image: string;
+    title: string;
+    href: string;
+  }[];
+} = {
+  description:
+    "Discover the possibility, power, and performance of AI and accelerated computing from the cloud to the edge across industries.",
+
+  industries: [
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy.coreimg.100.410.jpeg/1724862268124/industry-aeco.jpeg",
+      title: "Architecture, Engineering, Construction, and Operations",
+      href: "/industries/aeco",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy.coreimg.100.190.jpeg/1724862268174/industry-automotive.jpeg",
+      title: "Automotive",
+      href: "/industries/automotive",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy_383201393.coreimg.100.190.jpeg/1724862268226/industry-consumer-internet.jpeg",
+      title: "Consumer Internet",
+      href: "/industries/consumer-internet",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy_.coreimg.100.190.jpeg/1724862268277/industry-engery.jpeg",
+      title: "Energy",
+      href: "/industries/energy",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__918626233.coreimg.100.190.jpeg/1724862268329/industry-finance.jpeg",
+      title: "Financial Services",
+      href: "/industries/financial-services",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__305672767.coreimg.100.190.jpeg/1724862268378/industry-gaming.jpeg",
+      title: "Gaming",
+      href: "/industries/gaming",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__463653467.coreimg.100.190.jpeg/1724862268428/industry-public-sector-global.jpeg",
+      title: "Global Public Sector",
+      href: "/industries/global-public-sector",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__1077158884.coreimg.100.190.jpeg/1724862268477/industry-healthcare.jpeg",
+      title: "Healthcare and Life Sciences",
+      href: "/industries/healthcare-and-life",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__164259485.coreimg.100.190.jpeg/1724862268526/industry-higher-education.jpeg",
+      title: "Higher Education and Research",
+      href: "/industries/higher-education-and-research",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__636423336.coreimg.100.190.jpeg/1724862268577/industry-industrial.jpeg",
+      title: "Industrial",
+      href: "/industries/industrial",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__1349824919.coreimg.100.190.jpeg/1724862268630/industry-manufacturing.jpeg",
+      title: "Manufacturing",
+      href: "/industries/manufacturing",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__1349824919.coreimg.100.190.jpeg/1724862268630/industry-manufacturing.jpeg",
+      title: "Media and Entertainment",
+      href: "/industries/media-and-entertainment",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy.coreimg.100.410.jpeg/1724862268124/industry-aeco.jpeg",
+      title: "Architecture, Engineering, Construction, and Operations",
+      href: "/industries/aeco",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy.coreimg.100.190.jpeg/1724862268174/industry-automotive.jpeg",
+      title: "Automotive",
+      href: "/industries/automotive",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy_383201393.coreimg.100.190.jpeg/1724862268226/industry-consumer-internet.jpeg",
+      title: "Consumer Internet",
+      href: "/industries/consumer-internet",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy_.coreimg.100.190.jpeg/1724862268277/industry-engery.jpeg",
+      title: "Energy",
+      href: "/industries/energy",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__918626233.coreimg.100.190.jpeg/1724862268329/industry-finance.jpeg",
+      title: "Financial Services",
+      href: "/industries/financial-services",
+    },
+    {
+      image:
+        "https://www.nvidia.com/en-us/industries/home/_jcr_content/root/responsivegrid/nv_container_1389807/nv_container_copy/nv_teaser_copy_copy__305672767.coreimg.100.190.jpeg/1724862268378/industry-gaming.jpeg",
+      title: "Gaming",
+      href: "/industries/gaming",
+    },
+  ],
+};
--- src/app/industries/_components/industries-section/index.tsx
+++ src/app/industries/_components/industries-section/index.tsx
@@ -1,10 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { links } from "../navbar/data";
+import Link from "next/link";
+import { Card } from "@/components/ui/card";
+import { ChevronRight } from "lucide-react";
+import { data } from "./data";
 
 export default function IndustriesSection() {
   return (
     <section id={links[0].href.slice(1)}>
-      <main className="mx-auto w-full max-w-screen-2xl p-container"></main>
+      <main className="mx-auto w-full max-w-screen-2xl p-container">
+        <p className="mx-auto max-w-screen-lg text-balance text-center text-xl">
+          {data.description}
+        </p>
+        <div className="mt-8 grid auto-rows-[15rem] grid-cols-[repeat(auto-fill,minmax(17rem,1fr))] gap-4">
+          {data.industries.map((item, index) => (
+            <Link key={index} href={item.href}>
+              <Card className="group relative flex h-full w-full flex-col overflow-hidden bg-black/20 p-4 text-white shadow-xl">
+                <img
+                  src={item.image}
+                  alt={item.title}
+                  className="absolute inset-0 -z-10 h-full w-full object-cover transition-transform duration-500 group-hover:scale-110"
+                />
+                <div className="group mt-auto flex flex-wrap items-center gap-1">
+                  {item.title.split(" ").map((word, index) => (
+                    <span key={index}>{word}</span>
+                  ))}
+                  <ChevronRight className="text-primary brightness-125 transition-[color,transform] duration-500 group-hover:translate-x-1 group-hover:text-white" />
+                </div>
+              </Card>
+            </Link>
+          ))}
+        </div>
+      </main>
     </section>
   );
 }
